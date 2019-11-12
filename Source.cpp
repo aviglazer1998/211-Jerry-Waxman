1a731f71eeff8257ada93f7fc8156bce53b16d3b
Aglazer <51724278+aviglazer1998@users.noreply.github.com>
2019-11-12 00:29:45 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,140 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool ok(int q[], int c) {
+
+	for (int i = 0; i < c; i++) {
+		if ((q[c] == q[i] || ((c - i) == abs(q[c] - q[i]))))
+			return false;
+
+	}
+	return true;
+}
+
+void print(int q[], int& counter) {
+
+	counter++;
+
+	cout << "Solution #" << counter << endl;
+
+	int i, j, k, l;
+
+	typedef char box[5][7];
+
+	box bb, wb, * board[8][8];
+
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 7; j++) {
+
+			wb[i][j] = ' ';
+			bb[i][j] = char(219);
+		}
+	}
+
+	box bq = { {char(219),char(219),char(219),char(219),char(219),char(219),char(219)},
+
+				{char(219),' '      ,char(219),' '      ,char(219),' '      ,char(219)},
+
+				{char(219),' '      ,' '      ,' '      ,' '      ,' '      ,char(219)},
+
+				{char(219),' '      ,' '      ,' '      ,' '      ,' '      ,char(219)},
+
+				{char(219),char(219),char(219),char(219),char(219),char(219),char(219)} };
+
+	box wq = { {' '      ,' '      ,' '      ,' '      ,' '      ,' '      ,' '      },
+
+				{' '      ,char(219),' '      ,char(219),' '      ,char(219),' '      },
+
+				{' '      ,char(219),char(219),char(219),char(219),char(219),' '      },
+
+				{' '      ,char(219),char(219),char(219),char(219),char(219),' '      },
+
+				{' '      ,' '      ,' '      ,' '      ,' '      ,' '      ,' '      } };
+
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			if ((j + i) % 2 == 0) {
+				if (q[j] == i)
+					board[i][j] = &wq;
+				else
+					board[i][j] = &wb;
+			}
+
+			else {
+				if (q[j] == i) 
+					board[i][j] = &bq;
+				else 
+					board[i][j] = &bb;
+			}
+		}
+	}
+	cout << "   ";
+
+	for (i = 0; i < 7 * 8; i++) {
+
+		cout << '_';
+	}
+		cout << endl;
+
+	for (i = 0; i < 8; i++) {
+		for (k = 0; k < 5; k++) {
+
+			cout << "   " << char(179);
+
+			for (j = 0; j < 8; j++) {
+				for (l = 0; l < 7; l++) {
+
+					cout << (*board[i][j])[k][l];
+				}
+			}
+			cout << char(179) << endl;
+		}
+	}
+		cout << "   ";
+
+	for (i = 0; i < 7 * 8; i++) {
+
+		cout << char(196);
+	}
+		cout << endl;
+}
+
+void backtrack(int& c) {
+
+	c--;
+	if (c == -1)
+		exit(0);
+}
+
+int main() {
+
+	int c = 0, counter = 0, q[8];
+	q[0] = 0;
+
+	while (c != -1) {
+
+		c++;
+
+		if (c == 8) {
+			print(q, counter);
+			backtrack(c);
+		}
+		else 
+			q[c] = -1;
+
+		while (c != -1) {
+
+			q[c]++;
+
+			if (q[c] == 8)
+				backtrack(c);
+
+			else if
+				(ok(q, c) == true) 
+					break;
+		}
+	}
+	system("pause");
+	return 0;
+}
---
+++ Source1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	int col = 0,  board[8],  count = 1;
+	board[0] = 0;
+
+NC:
+	col++;
+	if (col == 8) 
+		goto Print;
+	board[col] = -1;
+NR:
+	board[col]++;
+	if (board[col] == 8)
+		goto Backtrack;
+	for (int i = 0; i < col; i++)
+		if (board[i] == board[col] || ((col - i) == abs( board[col] - board[i])))
+			goto NR;
+	goto NC;
+
+	board[col] = 1;
+	goto NR;
+
+Backtrack:
+	col--;
+	if (col == -1) {
+		system("pause");
+		return 0;
+	}
+	goto NR;
+
+Print:
+	cout << "Solution # " << count << endl;
+		for (int r = 0; r < 8; r++) {
+			cout << board[r]<< " ";
+		}
+		cout << endl << endl;
+
+	count++;
+	goto Backtrack;
+}
